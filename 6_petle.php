37f5b3a26106addc01fca7cd4050ff4d69a2dce8
Kameleonikos <kameleon.ptp@gmail.com>
2019-10-21 09:28:52 +0200

    mhm

---
+++ 6_petle.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style>
+      table, tr, td
+      {
+        border: solid 1px grey;
+        border-collapse: collapse;
+        text-align: center;
+        padding: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <table>
+      <?php
+      $i=1;
+      $j=1;
+      $s=0;
+      do {
+        echo "<tr>";
+        {
+          do {
+            if ($i==$j) {
+              echo "<td bgcolor = red>".$i*$j."</td>";
+              $s=$s+($i*$j);
+              $j++;
+            }
+            else {
+              echo "<td>".$i*$j."</td>";
+              $j++;
+            }
+          } while ($j <= 10);
+          $j=1;
+          echo "</tr>";
+          $i++;
+        }
+      } while ($i<=10); ?>
+    </table>
+    <?php echo "Suma elementów na przekątnej wynosi ". $s; ?>
+  </body>
+</html>

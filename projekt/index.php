a0bb9ac8ce553cdf87b6f5c71da1d453ea5d06cd
Kameleonikos <kameleon.ptp@gmail.com>
2019-11-05 22:12:48 +0100

    .

---
+++ projekt/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fiszkomani</title>
+    <link rel="stylesheet" href="master.css">
+  </head>
+  <body>
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
+    <div class="parallax1">
+
+    </div>
+    <div class="parallax2">
+
+    </div>
+    <footer>
+
+    </footer>
+  </body>
+</html>

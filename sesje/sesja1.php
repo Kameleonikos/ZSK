f004f7c0a082afa65655e173ba350aa4a8ffe682
Kameleonikos <kameleon.ptp@gmail.com>
2019-11-06 08:43:52 +0100

    .

---
+++ sesje/sesja1.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$_SESSION['name']='Janusz';
+?>
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Sesja</title>
+  </head>
+  <body>
+    Witany <?php echo $_SESSION['name']; ?> na stronie!<br>
+    Identyfikator sesji: <?php echo session_id(); ?><br>
+    <a href="./sesja1_1.php">Następna strona</a><br>
+  </body>
+</html>
